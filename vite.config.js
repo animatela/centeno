a20c2c51110c9d570bc41fc28b6f9145017ca06f
Monassi Carrasco <mon4ssi@outlook.com>
2023-06-06 04:52:23 -0500

    chore: add rollup options on build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+        // https://rollupjs.org/guide/en/#big-list-of-options
+        rollupOptions: {
+            output: {
+                chunkFileNames: 'assets/app-[hash].js',
+                assetFileNames: 'assets/app-[hash][extname]'
+            }
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.ts',
