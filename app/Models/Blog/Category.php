179996b24536b9627af11a0818b9e2aa1db68339
Monassi Carrasco <mon4ssi@outlook.com>
2023-06-06 19:47:13 -0500

    feat: add blog category resources

---
+++ app/Models/Blog/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Blog;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table = 'blog_categories';
+
+    /**
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'is_visible' => 'boolean',
+    ];
+}
