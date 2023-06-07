1f934bf7c543714beec4f8e74a9e859ba6ffc7ca
Monassi Carrasco <mon4ssi@outlook.com>
2023-06-07 16:23:23 -0500

    feat(workshop): add addresses to customer

--- app/Models/Workshop/Customer.php
+++ app/Models/Workshop/Customer.php
@@ -2,12 +2,19 @@
 
 namespace App\Models\Workshop;
 
+use App\Models\Address;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Customer extends Model
 {
     use HasFactory;
 
     protected $table = 'workshop_customers';
+
+    public function addresses(): MorphToMany
+    {
+        return $this->morphToMany(Address::class, 'addressable', 'addressables');
+    }
 }
