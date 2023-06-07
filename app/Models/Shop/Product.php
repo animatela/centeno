aa4d50084c58e2fe191ea41043a0ed771febd229
Monassi Carrasco <mon4ssi@outlook.com>
2023-06-07 13:19:12 -0500

    feat(shop): add products

---
+++ app/Models/Shop/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models\Shop;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Product extends Model implements HasMedia
+{
+    use HasFactory;
+    use InteractsWithMedia;
+
+    protected $table = 'shop_products';
+
+    /**
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'featured' => 'boolean',
+        'is_visible' => 'boolean',
+        'backorder' => 'boolean',
+        'requires_shipping' => 'boolean',
+        'published_at' => 'date',
+    ];
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class, 'shop_brand_id');
+    }
+}
