e449d99fbe58df0c02bfbbb997bfb8cc7869f4f3
Monassi Carrasco <mon4ssi@outlook.com>
2023-06-12 11:53:47 -0500

    feat(workshop): add customer relationship for users

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Workshop\Customer;
 use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -49,5 +51,10 @@ class User extends Authenticatable implements FilamentUser, MustVerifyEmail
     public function canAccessFilament(): bool
     {
         return str_ends_with($this->email, '@arcnet.dev') && $this->hasVerifiedEmail();
+    }
+
+    public function customer(): HasOne
+    {
+        return $this->hasOne(Customer::class, 'user_id');
     }
 }
