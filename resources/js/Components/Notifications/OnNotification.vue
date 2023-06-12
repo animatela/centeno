a14f818a2909674dce2a0f066f28da1015790a7d
Monassi Carrasco <mon4ssi@outlook.com>
2023-06-12 07:57:35 -0500

    feat: add notification components

---
+++ resources/js/Components/Notifications/OnNotification.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { NotificationGroup } from 'notiwind'
+
+import { NotifyGroupId } from '@/Composables/useNotification'
+
+import OnNotificationDefault from '@/Components/Notifications/OnNotificationDefault.vue'
+import OnNotificationFailed from '@/Components/Notifications/OnNotificationFailed.vue'
+import OnNotificationWarning from '@/Components/Notifications/OnNotificationWarning.vue'
+import OnNotificationSuccess from '@/Components/Notifications/OnNotificationSuccess.vue'
+</script>
+
+<template>
+    <div>
+        <NotificationGroup :group="NotifyGroupId.Info">
+            <div class="fixed inset-x-0 bottom-0 right-4 flex items-start justify-end p-6 px-4 py-6 pointer-events-none">
+                <div class="w-full max-w-sm">
+                    <OnNotificationDefault />
+                </div>
+            </div>
+        </NotificationGroup>
+
+        <NotificationGroup :group="NotifyGroupId.Success">
+            <div class="fixed inset-x-0 bottom-0 right-4 flex items-start justify-end p-6 px-4 py-6 pointer-events-none">
+                <div class="w-full max-w-sm">
+                    <OnNotificationSuccess />
+                </div>
+            </div>
+        </NotificationGroup>
+
+        <NotificationGroup :group="NotifyGroupId.Warning">
+            <div class="fixed inset-x-0 bottom-0 right-4 flex items-start justify-end p-6 px-4 py-6 pointer-events-none">
+                <div class="w-full max-w-sm">
+                    <OnNotificationWarning />
+                </div>
+            </div>
+        </NotificationGroup>
+
+        <NotificationGroup :group="NotifyGroupId.Failed">
+            <div class="fixed inset-x-0 bottom-0 right-4 flex items-start justify-end p-6 px-4 py-6 pointer-events-none">
+                <div class="w-full max-w-sm">
+                    <OnNotificationFailed />
+                </div>
+            </div>
+        </NotificationGroup>
+    </div>
+</template>
