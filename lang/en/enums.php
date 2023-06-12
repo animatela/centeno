8b6c3d2d55766b0d5f83b4a7358f9f7f27116143
Monassi Carrasco <mon4ssi@outlook.com>
2023-06-12 11:49:11 -0500

    feat: add translations for genders

--- lang/en/enums.php
+++ lang/en/enums.php
@@ -10,4 +10,10 @@ return [
     | The following language lines are used for enums.
     |
     */
+
+    'gender' => [
+        'male' => 'Male',
+        'female' => 'Female',
+        'other' => 'Other',
+    ],
 ];
