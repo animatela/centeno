c380201732c5a11fabd2ce4662a00ac34725d1b8
Monassi Carrasco <mon4ssi@outlook.com>
2023-06-12 22:30:26 -0500

    chore: remove item

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -41,12 +41,6 @@ const customerRoute = computed(() =>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Desde mantenimiento básico hasta reparaciones complejas, nos aseguraremos de que tu automóvil esté en las mejores manos.</p>
                     </a>
 
-
-                    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Servicios</h5>
-                        <p class="font-normal text-gray-700 dark:text-gray-400">Nuestros servicios de mantenimiento automotriz incluyen tanto el mantenimiento preventivo como el correctivo.</p>
-                    </a>
-
                     <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Vehículos</h5>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Registra tu vehículo con nosotros y experimenta un servicio automotriz de calidad y personalizado.</p>
