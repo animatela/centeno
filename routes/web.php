09047654e6ec1745eaad5ccb1fd3fff0d661881b
Monassi Carrasco <mon4ssi@outlook.com>
2023-06-12 22:22:07 -0500

    fix: missing controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CustomerInformationController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
