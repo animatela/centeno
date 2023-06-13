8439505d4437c7edc2f571bac01cb97fa5f26eb4
Monassi Carrasco <mon4ssi@outlook.com>
2023-06-12 22:39:32 -0500

    chore: use router link

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -39,15 +39,15 @@ const customerRoute = computed(() =>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Valoramos tu privacidad y te ofrecemos la opción de gestionar tu información personal de manera segura y confidencial.</p>
                     </Link>
 
-                    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <Link href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Reservas</h5>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Desde mantenimiento básico hasta reparaciones complejas, nos aseguraremos de que tu automóvil esté en las mejores manos.</p>
-                    </a>
+                    </Link>
 
-                    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <Link href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Vehículos</h5>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Registra tu vehículo con nosotros y experimenta un servicio automotriz de calidad y personalizado.</p>
-                    </a>
+                    </Link>
                 </div>
 
             </div>
