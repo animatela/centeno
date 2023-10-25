f61f2e225a5a2068a157afa040b80feeaf5d72f9
Monassi Carrasco <mon4ssi@outlook.com>
2023-10-25 00:17:29 -0500

    fix: prevent error for mailtrap

--- config/mail.php
+++ config/mail.php
@@ -43,6 +43,7 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
+            'verify_peer' => false, // <== This is needed here
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
